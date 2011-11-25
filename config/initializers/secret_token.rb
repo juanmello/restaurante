33c2c4569c268defddeab255bceab71b82b475dd
Juan Mello <juan.ferdez.poa@gmail.com>
2011-11-25 19:34:56 -0200

    TESTANDO CONHECIMENTOS RUBY ON RAILS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = 'f07070ad5e8df32f0a86e0131a5981c58fdc4f2fb7c0deb364373085c0fbc187dace6ddeb6e24203e4e43ff58401658e0d9b91667e37d5dc2f2564d2a49ebbb8'
